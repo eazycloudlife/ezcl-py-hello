f39c41c98a95d20539b47b611f0781abb94694d5
Eazy Cloud Life (EZCL) <easycloudlife@gmail.com>
2024-07-14 17:41:40 +0530

    initial

---
+++ hello/src/world.py
@@ -0,0 +1,42 @@
+
+def hello():
+    """
+    In return, Hello!.
+
+    """
+    return "Hello!"
+
+def hello(name):
+    """
+    In return, Hello, name!.
+
+    """
+    return f"Hello, {name}!"
+
+def hello_world():
+    """
+    In return, Hello, World!.
+
+    """
+    return "Hello, World!"
+
+def hi():
+    """
+    In return, Hi!.
+
+    """
+    return "Hi!"
+
+def hi(name):
+    """
+    In return, Hello, name!.
+
+    """
+    return f"Hi, {name}"
+
+def hi_world():
+    """
+    In return, Hi, World!.
+
+    """
+    return "Hi, World!"
